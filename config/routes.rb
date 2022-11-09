b8f5e6060883153e85e2508d3af59ce2e8a77739
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-11-09 05:04:44 +0000

    [Add} deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  # ユーザー側
+  # UEL / users/sign_in ..
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
