c6aeeaa8cb19a39a8217144c72aae44526c89c95
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-11-11 09:13:43 +0000

    [Update] 項目の保存機能作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,11 +1,24 @@
 class Public::ItemsController < ApplicationController
 
+  def create
+    item = current_user.items.new(item_params)
+    item.save
+    redirect_to item_path(item.id)
+  end
+
   def index
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :elapsed_time, :start_time, :end_time, :tag_id)
   end
 end
